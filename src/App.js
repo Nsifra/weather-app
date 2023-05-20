ba55a59b3b486ec36d8941ec8fca1ef1b7bdd44c
Nsifra <snavagao@nyit.edu>
2023-05-19 22:27:08 -0400

    added css

---
+++ src/App.js
@@ -0,0 +1,71 @@
+
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import CityInput from './components/cityinput';
+import CurrentWeather from './components/currentweather';
+import Forecast from './components/forecast';
+
+const API_KEY = 'e8a4be8f59a7aeeeabc66fe27bf463a4';
+
+const WeatherApp = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState([]);
+
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  const fetchForecast = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=imperial`
+      );
+      const data = await response.json();
+      const formattedData = data.list.map((item) => ({
+        date: item.dt_txt,
+        temperature: item.main.temp,
+        description: item.weather[0].description,
+      }));
+      setForecastData(formattedData);
+    } catch (error) {
+      console.error('Error fetching forecast data:', error);
+    }
+  };
+
+  const handleCitySubmit = (city) => {
+    setCity(city);
+    fetchWeather();
+    fetchForecast();
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col md={6}>
+          <h1 className="text-center">Weather App</h1>
+          <CityInput onCitySubmit={handleCitySubmit} />
+          {weatherData && weatherData.main && (
+            <CurrentWeather
+              city={weatherData.name}
+              temperature={weatherData.main.temp}
+              description={weatherData.weather[0].description}
+            />
+          )}
+          {forecastData.length > 0 && <Forecast forecastData={forecastData} />}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default WeatherApp;
+
